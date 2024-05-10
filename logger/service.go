21e17d18331eda50295d8794ea860e24b9d74eed
nwleedev <nw.lee@outlook.com>
2024-05-10 18:42:35 +0900

    refactor: logger methods

--- logger/service.go
+++ logger/service.go
@@ -3,11 +3,14 @@ package logger
 import (
 	"fmt"
 	"log"
+	"runtime"
+	"time"
 )
 
 type LoggerService interface {
-	Println(format string, v ...any)
-	PrintError(format string, err error)
+	Info(values ...any)
+	Warn(message string, values ...any)
+	Error(err error, message string, values ...any)
 }
 
 type Service struct {
@@ -20,18 +23,40 @@ func NewService(logger *log.Logger) LoggerService {
 	return service
 }
 
-func (service *Service) Println(format string, v ...any) {
-	if len(v) == 0 {
-		service.logger.Println(format)
-	}
-	row := fmt.Sprintf(format, v...)
-	service.logger.Printf("%s\n", row)
+func (service *Service) Info(values ...any) {
+	runtimeCaller, _, _, _ := runtime.Caller(1)
+
+	callerName := runtime.FuncForPC(runtimeCaller).Name()
+	now := time.Now()
+	createdAt := now.Format(time.DateTime)
+	title := "INFO"
+	infoBody := fmt.Sprintln(values...)
+
+	log.Printf("%s %s %s %s\n", title, createdAt, callerName, infoBody)
 }
 
-func (service *Service) PrintError(format string, err error) {
-	prefix := format
-	if format == "" {
-		prefix = "Error: "
-	}
-	service.logger.Printf("%s %v\n", prefix, err)
+func (service *Service) Warn(message string, values ...any) {
+	runtimeCaller, _, _, _ := runtime.Caller(1)
+
+	callerName := runtime.FuncForPC(runtimeCaller).Name()
+	now := time.Now()
+	createdAt := now.Format(time.DateTime)
+	title := "WARN"
+	warnMessage := fmt.Sprintln(values...)
+	warnBody := fmt.Sprintf("\n---\nMessage: %s %s---", message, warnMessage)
+
+	log.Printf("%s %s %s %s\n", title, createdAt, callerName, warnBody)
+}
+
+func (service *Service) Error(err error, message string, values ...any) {
+	runtimeCaller, _, _, _ := runtime.Caller(1)
+
+	callerName := runtime.FuncForPC(runtimeCaller).Name()
+	now := time.Now()
+	createdAt := now.Format(time.DateTime)
+	title := "ERROR"
+	errorMessage := fmt.Sprintln(values...)
+	errorBody := fmt.Sprintf("\n---\nMessage: %s %s%s\n---", message, errorMessage, err)
+
+	log.Printf("%s %s %s\n%s\n", title, createdAt, callerName, errorBody)
 }
